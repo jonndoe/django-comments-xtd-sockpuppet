db89264583e517834ac267b39d21592822fd761e
jonndoe <user@mail.com>
2021-05-22 12:45:27 +0100

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,26 @@
+import { Application } from 'stimulus'
+import StimulusReflex from 'stimulus_reflex'
+import WebsocketConsumer from 'sockpuppet-js'
+//import RenderallpostsController from '../posts/javascript/controllers/renderallposts_controller'
+
+
+
+import "./app.css";
+import "./bootstrap.min.css";
+
+
+//const application = Application.start()
+//const consumer = new WebsocketConsumer(`ws://${window.location.host}/ws/sockpuppet-sync`)
+
+//application.register("showgoodposts", ShowgoodpostsController)
+//application.register("showcounterstring", ShowcounterstringController)
+//application.register("renderallposts", RenderallpostsController)
+//StimulusReflex.initialize(application, { consumer })
+
+
+console.log("hello, world!");
+console.log("good good good")
+
+
+//document.getElementById("goodid").innerHTML = "Hello from Webpack! I'm refreshing automatically!";
+
