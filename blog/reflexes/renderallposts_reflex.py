6479b7d100b6dc8d5e2d41dc7e8f17e794efd12f
jonndoe <user@mail.com>
2021-05-24 09:27:15 +0100

    edited renderallposts_reflex.py, deleted some garbage

--- blog/reflexes/renderallposts_reflex.py
+++ blog/reflexes/renderallposts_reflex.py
@@ -1,30 +1,22 @@
 from sockpuppet.reflex import Reflex
-from blog.models import Post
+
 
 # for debugging
 from urllib.parse import urlparse
-from django.urls import resolve
 
 
+# As far as I get the idea, One reflex should be connected with One View.
 class RenderallpostsReflex(Reflex):
 
     def renderfrompaginator(self):
         print('renderfrompaginator method of Reflex invoked!!!')
-        #print('dataset is:', self.element.dataset)
         self.pagetogo = self.element.dataset['pagetogo']
 
         parsed_url = urlparse(self.url)
-        print('parset_url.query is: ', parsed_url.query)
         current_view_path = '/blog/renderallposts/'
         if parsed_url.path != '/blog/renderallposts/':
-            #if parsed_url.query != '':
-                #query = '?'
-            #else:
-                #query = ''
             new_url = parsed_url.scheme + '://' + parsed_url.netloc + current_view_path + parsed_url.params + self.pagetogo + parsed_url.fragment
             self.url = new_url
-
-
 
     def render(self):
         print('render method of Reflex invoked!!!')
@@ -33,43 +25,25 @@ class RenderallpostsReflex(Reflex):
         # based on the name of the reflex, and also knowledge of the
         # line in urls.py which reffers to the corresponding view.
         # i.e. whatever reflex.url is now, it should be 'http://localhost:8000/blog/renderallposts/'
-        # i.e
 
-        #print('self.url is: ', self.url)
         parsed_url = urlparse(self.url)
-        #print('parsed url is: ', parsed_url)
-        #print('parsed url path is: ', parsed_url.path)
 
+        # This method has to trigger the view which can be riched with the following path:
         # hardcoded for now, but we should get this path programmatically
         current_view_path = '/blog/renderallposts/'
 
         if parsed_url.path != '/blog/renderallposts/':
-            #print('parsed_url.path is wrong!!!!! :', parsed_url.path)
-            #parsed_url.path = '/blog/renderallposts/'
             # todo: create new self.url taking into account parsed_url.path
             if parsed_url.query != '':
                 query = '?'
             else:
                 query = ''
             new_url = parsed_url.scheme + '://' + parsed_url.netloc + current_view_path + parsed_url.params + query + parsed_url.query + parsed_url.fragment
-            #print('new_url is : ', new_url)
             self.url = new_url
-            parsed_url = urlparse(self.url)
 
-
-
-
-        #resolved = resolve(parsed_url.path)
-        #print('resolved url is: ', resolved)
-
-        #print(self)
-        #view = resolved.func.view_class()
-        #print('view is :', view)
-        #view.request = self.request
-        #print('view request is :', view.request)
-
-        # Here we will not take any posts objects since
+        # Here we will not take any posts objects from db since
         # ListView will do it automatically for us. But we can if we need to,
         # for example:
-        #posts = Post.objects.all()
-        #self.posts = posts
+        # from blog.models import Post
+        # posts = Post.objects.all()
+        # self.posts = posts
